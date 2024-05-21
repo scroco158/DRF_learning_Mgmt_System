2b753b9ab71bbadabfbcdc15d5ba8ac6a96eae2c
scroco158 <scroco@mail.ru>
2024-05-21 13:47:04 +0300

    lms | создал приложение и модели курс и урок black

--- users/models.py
+++ users/models.py
@@ -34,6 +34,6 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-        ordering = ['email']
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["email"]
