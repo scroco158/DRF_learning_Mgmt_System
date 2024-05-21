80239ef3b93cfac0a71f95fa2ac6e69a08851e7f
scroco158 <scroco@mail.ru>
2024-05-21 15:53:56 +0300

    lms | CRUD Generic Lesson

--- lms/urls.py
+++ lms/urls.py
@@ -1,6 +1,8 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
-from lms.views import CourseViewSet
+from lms.views import CourseViewSet, LessonListAPIView, LessonCreateAPIView, LessonUpdateAPIView, LessonDestroyAPIView, LessonRetrieveAPIView
 from .apps import LmsConfig
+
 
 app_name = LmsConfig.name
 
@@ -9,5 +11,11 @@ router = SimpleRouter()
 router.register("", CourseViewSet)
 
 
-urlpatterns = []
+urlpatterns = [
+    path("lessons/", LessonListAPIView.as_view(), name="lesson-list"),
+    path("lessons/<int:pk>", LessonRetrieveAPIView.as_view(), name="lesson-detail"),
+    path("lessons/create/", LessonCreateAPIView.as_view(), name="lesson-create"),
+    path("lessons<int:pk>/", LessonDestroyAPIView.as_view(), name="lesson-destroy"),
+    path("lessons<int:pk>/", LessonUpdateAPIView.as_view(), name="lesson-update"),
+]
 urlpatterns += router.urls
