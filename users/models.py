91acce3a7862ba395c6924966e00c63cc072e016
scroco158 <scroco@mail.ru>
2024-05-24 21:12:18 +0300

    users | модель платежей

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from lms.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -37,3 +39,21 @@ class User(AbstractUser):
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
         ordering = ["email"]
+
+
+class Payment(models.Model):
+    """ Оплата за курс или урок"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    amount = models.PositiveIntegerField(verbose_name='Сумма оплаты')
+    p_date = models.DateField(verbose_name='Дата оплаты')
+    сourse = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='За курс', null=True, blank=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='За урок', null=True, blank=True)
+    method = models.CharField(max_length=20, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return self.method
+
+    class Meta:
+        verbose_name = 'Оплата'
+        verbose_name_plural = 'Оплата'
+
