46e8cc4d049e6637fc7a7e8390ff9cc9d8a72643
scroco158 <scroco@mail.ru>
2024-05-25 08:24:02 +0300

    users | payment | добавил в админку

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from users.models import User, Payment
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone', 'city', 'avatar')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'p_date', 'course', 'lesson', 'method')
+
