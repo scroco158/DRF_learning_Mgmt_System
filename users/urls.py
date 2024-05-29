539f82386bfbf04ff21f51d094e7c9559db49a66
scroco158 <scroco@mail.ru>
2024-05-29 17:01:42 +0300

    User | аутентиф. | настройка urls токенов

--- users/urls.py
+++ users/urls.py
@@ -5,12 +5,20 @@ from .apps import UsersConfig
 from .views import CourseViewSet, UserListAPIView, UserRetrieveAPIView, UserCreateAPIView, UserUpdateAPIView, \
     UserDestroyAPIView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 app_name = UsersConfig.name
 router = SimpleRouter()
 router.register("", CourseViewSet)
 
 
 urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('users/', UserListAPIView.as_view(), name='user-list'),
     path('users/<int:pk>', UserRetrieveAPIView.as_view(), name='user-detail'),
     path('users/create/', UserCreateAPIView.as_view(), name='user-create'),
