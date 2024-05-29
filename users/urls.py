278f3909be5a8fcd2dc255f9cf5ac7fa69aa6767
scroco158 <scroco@mail.ru>
2024-05-29 16:30:10 +0300

    User | CRUD | urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views import View
 from rest_framework.routers import SimpleRouter
 from .apps import UsersConfig
-from .views import CourseViewSet
+from .views import CourseViewSet, UserListAPIView, UserRetrieveAPIView, UserCreateAPIView, UserUpdateAPIView, \
+    UserDestroyAPIView
 
 app_name = UsersConfig.name
 router = SimpleRouter()
@@ -9,5 +11,10 @@ router.register("", CourseViewSet)
 
 
 urlpatterns = [
+    path('users/', UserListAPIView.as_view(), name='user-list'),
+    path('users/<int:pk>', UserRetrieveAPIView.as_view(), name='user-detail'),
+    path('users/create/', UserCreateAPIView.as_view(), name='user-create'),
+    path('users/<int:pk>', UserUpdateAPIView.as_view(), name='user-update'),
+    path('users/<int:pk>', UserDestroyAPIView.as_view(), name='user-destroy'),
 ]
 urlpatterns += router.urls
