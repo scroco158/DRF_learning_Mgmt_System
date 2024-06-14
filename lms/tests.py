5c9bfd6c7f7119ee0f93504689d425ac0b612a19
scroco158 <scroco@mail.ru>
2024-06-14 16:12:06 +0300

    тест с ошибкой

--- lms/tests.py
+++ lms/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from lms.models import Course, Lesson, Subscription
+from users.models import User
+
+
+class LessonTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email="test@user.com")
+        self.course = Course.objects.create(name="Test_Course", description="Курс для теста")
+        self.lesson = Lesson.objects.create(name="Lesson_1", description="Урок для теста", course=self.course, owner=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_lesson_retrieve(self):
+        url = reverse("lms:lesson-detail", args=(self.lesson.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get("name"), self.lesson.name
+        )
+
+    def test_lesson_create(self):
+        url = reverse("lms:lesson-create")
+        data = {
+            "name": "Lesson_2"
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED
+        )
+        self.assertEqual(
+            Lesson.objects.all().count(), 2
+        )
